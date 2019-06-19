c1af70d2b2f162217c98fbf0148965be3ab88dbf
Alla Panova <alla.mixalvna@gmail.com>
2019-06-19 23:47:54 +0300

    выполнено задание одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,86 @@
 'use strict';
 var NUMBER_OF_WIZARDS = 4;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var userDialog = document.querySelector('.setup');
 var openUserDialog = document.querySelector('.setup-open');
-var closeUserDialog = document.querySelector('.setup-close');
+var closeUserDialog = userDialog.querySelector('.setup-close');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupWizard = document.querySelector('.setup-wizard');
+var setupUserName = userDialog.querySelector('.setup-user-name');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+var setupPleer = document.querySelector('.setup-player');
+var inputCoatColor = setupPleer.querySelector('input[name = coat-color]');
+var inputEyesColor = setupPleer.querySelector('input[name = eyes-color]');
+var inputFireballColor = wizardFireball.querySelector('input[name = fireball-color]');
 
-openUserDialog.addEventListener('click', function () {
+var randomInteger = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+var openPopup = function () {
   userDialog.classList.remove('hidden');
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+};
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+openUserDialog.addEventListener('click', openPopup);
+
+closeUserDialog.addEventListener('click', closePopup);
+
+closeUserDialog.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
 });
 
-closeUserDialog.addEventListener('click', function () {
-  userDialog.classList.add('hidden');
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+document.addEventListener('keydown', onPopupEscPress);
+
+setupUserName.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onPopupEscPress);
+});
+
+setupUserName.addEventListener('blur', function () {
+  document.addEventListener('keydown', onPopupEscPress);
+});
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = coatColors[randomInteger(0, 5)];
+  inputCoatColor.value = wizardCoat.style.fill;
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = eyesColors[randomInteger(0, 4)];
+  inputEyesColor.value = wizardEyes.style.fill;
+});
+
+wizardFireball.addEventListener('click', function () {
+  var color = fireballColors[randomInteger(0, 4)];
+  wizardFireball.style.backgroundColor = color;
+  inputFireballColor.value = color;
 });
 
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -20,14 +90,6 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
-var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var randomInteger = function (min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-};
 
 var getWizardsArray = function (numberOfWizards) {
   var wizards = [];
