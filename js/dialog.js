aa5e81a2050d117e3b230d3f6165974f5c391116
Alla Panova <alla.mixalvna@gmail.com>
2019-06-23 21:43:52 +0300

    добавлено перетаскивание звездочки в рюкзак

--- js/dialog.js
+++ js/dialog.js
@@ -110,4 +110,47 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
+
+  /* управление перетаскиванием звездочек в рюкзак*/
+
+  var star = document.querySelector('img[draggable = "true"]');
+
+  star.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    star.style.position = 'absolute';
+    star.style.zIndex = 100;
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      star.style.top = (star.offsetTop - shift.y) + 'px';
+      star.style.left = (star.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
